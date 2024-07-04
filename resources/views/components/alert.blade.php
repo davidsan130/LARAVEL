5cbd9922d054f25047ffae271deeb125ebc82629
David Ruiz <davidsantiagoruizvalencia@gmail.com>
2024-07-04 14:31:06 -0500

    Inicio

---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,44 @@
+@props(['type'])
+
+@php
+    switch ($type) {
+
+        case 'info':
+
+            $class = 'text-blue-800 bg-blue-50 dark:bg-gray-800 dark:text-blue-400';
+
+            break;
+        
+        case 'danger':
+
+            $class = 'text-red-800 bg-blue-50 dark:bg-gray-800 dark:text-red-400';
+
+            break;
+
+        case 'success':
+
+            $class = 'text-green-800 bg-green-50 dark:bg-gray-800 dark:text-green-400';
+
+            break;
+
+        case 'warning':
+
+            $class = 'text-yellow-800 bg-blue-50 dark:bg-gray-800 dark:text-yellow-400';
+            
+            break;
+
+        case 'dark':
+            
+            $class = 'text-gray-800 bg-blue-50 dark:bg-gray-800 dark:text-gray-400';
+
+            break;
+        
+        default:
+            $class = 'text-blue-800 bg-blue-50 dark:bg-gray-800 dark:text-blue-400';
+            break;
+    }
+@endphp
+
+<div class="p-4 mb-4 text-sm rounded-lg {{$class}}" role="alert">
+    <span class="font-medium">{{ $title ?? 'Info!' }}</span> {{ $slot }}
+</div>
